a423156ffd806fb8d82f806a712ca5eba1d0ae92
Shamak10 <124183731+Shamak10@users.noreply.github.com>
2023-02-24 12:53:58 -0500

    Update countingDownByTwo.java

--- src/countingDownByTwo.java
+++ src/countingDownByTwo.java
@@ -1,5 +1,8 @@
-public class Main {
+public class countingDownByTwo {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("");
+        for (int i = 10; i >= 0; i -= 2) { // counts down from 10 to 0
+            System.out.println(i + " "); // prints the current count
+        }
     }
 }
